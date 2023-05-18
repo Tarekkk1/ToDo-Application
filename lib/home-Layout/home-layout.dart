4dd290287d40825aba75872219c64937d90b27d6
Tarek <tarekzeyad31@gmail.com>
2023-05-18 14:12:03 +0300

    strature formating

---
+++ lib/home-Layout/home-layout.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class HomeLayout extends StatelessWidget {
+ static const String routeName ="Homelayout";
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
